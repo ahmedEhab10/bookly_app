497962ae34320e195992e7980a4de61d1a671906
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-10-16 14:08:02 +0300

    add animation to splash view

--- lib/Features/Splash/Presentaion/View/widgets/Splash_view_body.dart
+++ lib/Features/Splash/Presentaion/View/widgets/Splash_view_body.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> scrollAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 962));
+
+    scrollAnimation = Tween<Offset>(begin: Offset(0, 1), end: Offset(0, 0))
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset('assets/images/Logo.png'),
-    );
+    return AnimatedBuilder(
+        animation: scrollAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: scrollAnimation,
+            child: Center(
+              child: Image.asset('assets/images/Logo.png'),
+            ),
+          );
+        });
   }
 }
