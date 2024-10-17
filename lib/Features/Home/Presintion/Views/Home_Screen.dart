aeec277a50b2c689c23532dec2e7744c45c1fc5d
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-10-17 09:16:42 +0300

    make custom list view item

--- lib/Features/Home/Presintion/Views/Home_Screen.dart
+++ lib/Features/Home/Presintion/Views/Home_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/Home/Presintion/Views/Widgets/Custom_App_Bar.dart';
+import 'package:bookly_app/Features/Home/Presintion/Views/Widgets/Custom_List_View_Item.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
-        children: [CustomAppBar()],
+        children: [CustomAppBar(), CustomListViewItem()],
       ),
     );
   }
