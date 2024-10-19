4460dd826131d609e1ab3fb05b085d8dad2f2a55
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-10-19 21:28:43 +0300

    create styles

--- lib/Features/Home/Presintion/Views/Home_Screen.dart
+++ lib/Features/Home/Presintion/Views/Home_Screen.dart
@@ -26,9 +26,75 @@ class HomeScreen extends StatelessWidget {
               'Best Sellar',
               style: Style.TitleMedum,
             ),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerListViewItem()
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 100,
+          child: AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/Haary potter test.jpeg'),
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 25,
+        ),
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Harry Potter\nand the Goblet of Fire',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+            Text('1K Rowling',
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.grey)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '19.99 \$',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  width: 159,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                  size: 16,
+                ),
+                Text(
+                  '3.8',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
