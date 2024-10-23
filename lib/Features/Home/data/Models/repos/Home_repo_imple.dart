c75404faf7c72a29bb3ee5c919c0d3071a070c68
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-10-23 15:17:31 +0300

    featch beast seller books

--- lib/Features/Home/data/Models/repos/Home_repo_imple.dart
+++ lib/Features/Home/data/Models/repos/Home_repo_imple.dart
@@ -1,13 +1,25 @@
+import 'package:bookly_app/Core/Utils/Api_Service.dart';
 import 'package:bookly_app/Core/errors/failuers.dart';
 import 'package:bookly_app/Features/Home/data/Models/book_model/book_model.dart';
 import 'package:bookly_app/Features/Home/data/Models/repos/Home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImple implements HomeRepo {
+  ApiService apiService;
+  HomeRepoImple(this.apiService);
   @override
-  Future<Either<Failuers, List<BookModel>>> featchBeastSellerBooks() {
-    // TODO: implement featchBeastSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failuers, List<BookModel>>> featchBeastSellerBooks() async {
+    try {
+      List<BookModel> Books = [];
+      var data = await apiService.getallBook(
+          endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+      for (var item in data['items']) {
+        Books.add(BookModel.fromJson(item));
+      }
+      return right(Books);
+    } catch (e) {
+      return left(servier_failuer());
+    }
   }
 
   @override
