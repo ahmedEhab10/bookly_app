4e6719cf2cfd59fd38a06334c8d00466591bd024
ahmedEhab10 <ahmedhobaa24@gmail.com>
2024-10-25 15:57:37 +0300

    create future books methoud

--- lib/Features/Home/Presintion/View_Models/Future_Books_Cubit/future_books_cubit.dart
+++ lib/Features/Home/Presintion/View_Models/Future_Books_Cubit/future_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/Home/data/Models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/Models/repos/Home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'future_books_state.dart';
 
 class FutureBooksCubit extends Cubit<FutureBooksState> {
-  FutureBooksCubit() : super(FutureBooksInitial());
+  FutureBooksCubit({required this.homeRepo}) : super(FutureBooksInitial());
+  HomeRepo homeRepo;
+
+  Future<void> featchfeatureBooks() async {
+    emit(FutureBooksLoading());
+    var result = await homeRepo.featchfeatureBooks();
+    result.fold(
+      (fulier) {
+        emit(FutureBooksfailuer(fulier.errMessage));
+      },
+      (Books) {
+        emit(FutureBooksSuccess(Books));
+      },
+    );
+  }
 }
